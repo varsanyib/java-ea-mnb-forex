ee006e23fa8439930088fd18dc64faec37e37b41
Varsányi Barnabás <varsanyib0125@gmail.com>
2025-11-07 22:49:21 +0100

    Add basic controllers and templates for MNB project

---
+++ SpringLectureProject/src/main/java/hu/gamf/springlectureproject/MainController.java
@@ -0,0 +1,18 @@
+package hu.gamf.springlectureproject;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.Date;
+
+@Controller
+public class MainController {
+
+    @GetMapping("/")
+    public String Root(Model model) {
+        model.addAttribute("nowDate", new Date());
+        return "index";
+    }
+
+}
