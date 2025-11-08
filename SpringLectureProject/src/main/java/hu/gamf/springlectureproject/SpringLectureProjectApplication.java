d9f2cc298f93e260f51e6c6770dd9e629010721a
Varsányi Barnabás <varsanyib0125@gmail.com>
2025-11-08 01:31:42 +0100

    Integrate dotenv-java for environment variable management
    Added dotenv-java dependency and EnvConfig configuration to load environment variables from a .env file. Updated Config.java to retrieve sensitive values (ACCOUNT_ID and ACCOUNT_TOKEN) from environment variables instead of hardcoding. This improves security and flexibility for managing configuration.

--- SpringLectureProject/src/main/java/hu/gamf/springlectureproject/SpringLectureProjectApplication.java
+++ SpringLectureProject/src/main/java/hu/gamf/springlectureproject/SpringLectureProjectApplication.java
@@ -1,7 +1,10 @@
 package hu.gamf.springlectureproject;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
 @SpringBootApplication
 public class SpringLectureProjectApplication {
@@ -11,3 +14,4 @@ public class SpringLectureProjectApplication {
     }
 
 }
+
--- SpringLectureProject/src/main/java/hu/gamf/springlectureproject/tools/Config.java
+++ SpringLectureProject/src/main/java/hu/gamf/springlectureproject/tools/Config.java
@@ -1,8 +1,11 @@
 package hu.gamf.springlectureproject.tools;
 import com.oanda.v20.account.AccountID;
+import io.github.cdimascio.dotenv.Dotenv;
 
 public class Config {
+    private static final Dotenv dotenv = Dotenv.load();
+
     public static final String URL = "https://api-fxpractice.oanda.com";
-    //public static final AccountID ACCOUNTID = new AccountID("<Account ID>");
-    //public static final String TOKEN = "<TOKEN>";
+    public static final AccountID ACCOUNTID = new AccountID(dotenv.get("ACCOUNT_ID"));
+    public static final String TOKEN = dotenv.get("ACCOUNT_TOKEN");
 }
